8a9aba78a379269ccaf053c3dd49afff9d27af73
ayman adas <139353510+ayman-adas@users.noreply.github.com>
2023-11-17 16:45:58 +0300

    Update c_button_row.dart

--- calculator/lib/contoller/c_button_row.dart
+++ calculator/lib/contoller/c_button_row.dart
@@ -1,6 +1,7 @@
 import 'package:calculator/widget/w_button.dart';
 import 'package:flutter/material.dart';
 
+// 4 button in one row
 class CButtonRow extends StatelessWidget {
   const CButtonRow(
       {super.key,
@@ -27,21 +28,22 @@ class CButtonRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(5.0),
+      padding: const EdgeInsets.all(7.0),
       child: Row(
         children: [
+          //first button
           WButton(text: text1, click: click1),
           const SizedBox(
             width: 10,
-          ),
+          ), //second button
           WButton(text: text2, click: click2),
           const SizedBox(
             width: 10,
-          ),
+          ), //third button
           WButton(text: text3, click: click3),
           const SizedBox(
             width: 10,
-          ),
+          ), //forth button
           WButton(text: text4, click: click4)
         ],
       ),
