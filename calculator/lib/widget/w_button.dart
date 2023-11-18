84aa55479cba160a65f9661370114fa51006fb6f
ayman adas <139353510+ayman-adas@users.noreply.github.com>
2023-11-18 13:19:49 +0300

    Update w_button.dart

--- calculator/lib/widget/w_button.dart
+++ calculator/lib/widget/w_button.dart
@@ -1,29 +1,30 @@
 import 'package:flutter/material.dart';
 
-// button
+// for button and make its style and color
 class WButton extends StatelessWidget {
-  const WButton({super.key, required this.text, required this.click});
+  const WButton({
+    Key? key,
+    required this.text,
+    required this.click,
+  }) : super(key: key);
+
   final String text;
   final void Function() click;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 2),
       child: ElevatedButton(
-        //clickback function
         onPressed: click,
-        style: const ButtonStyle(
-            //background color for button
-            backgroundColor:
-                MaterialStatePropertyAll(Color.fromARGB(234, 85, 79, 79)),
-            //foreground color for text in button
-            foregroundColor:
-                MaterialStatePropertyAll(Color.fromARGB(255, 0, 255, 8)),
-            // the shape of button is circular
-            shape: MaterialStatePropertyAll(CircleBorder()),
-            //size of button
-            minimumSize: MaterialStatePropertyAll(Size(85, 85))),
-        //text will be release in button
+        style: ButtonStyle(
+          backgroundColor:
+              MaterialStateProperty.all(const Color.fromARGB(234, 85, 79, 79)),
+          foregroundColor:
+              MaterialStateProperty.all(const Color.fromARGB(255, 0, 255, 8)),
+          shape: MaterialStateProperty.all(const CircleBorder()),
+          minimumSize: MaterialStateProperty.all(const Size(85, 85)),
+        ),
         child: Text(
           text,
           style: const TextStyle(fontSize: 30),
